003f8cae9e2cbdc1900175d455596e49b8744868
felixthescar <felixscarmoc2@protonmail.com>
2023-01-13 17:27:25 +0200

    Update createDb.sql

--- createDb.sql
+++ createDb.sql
@@ -27,15 +27,19 @@ CREATE TABLE Account(
 
 ALTER TABLE Account ADD CONSTRAINT k1 FOREIGN KEY (cn_angajat_id_fk) REFERENCES Angajat(an_id);
 ALTER TABLE Account ADD CONSTRAINT k2 FOREIGN KEY (cn_client_id_fk) REFERENCES Client(cl_id);
+DELETE FROM account;
 
-INSERT INTO Angajat VALUES ('Ion','Ionut',		   '1111111111111', '0722222222', 2500);
-INSERT INTO Angajat VALUES ('Marinescu','Marian',  '2222222222222', '0733333333', 2400);
-INSERT INTO Angajat VALUES ('Stefanescu','Stefan', '3333333333333', '0744444444', 2300);
+DELETE FROM angajat;
+INSERT INTO angajat (an_id, an_name, an_surname, an_num_personal_code, an_phone_nr, an_salary) VALUES (1, 'Ion','Ionut', '1111111111111', '0722222222', 2500);
+INSERT INTO Angajat (an_id, an_name, an_surname, an_num_personal_code, an_phone_nr, an_salary) VALUES (2, 'Marinescu','Marian',  '2222222222222', '0733333333', 2400);
+INSERT INTO Angajat (an_id, an_name, an_surname, an_num_personal_code, an_phone_nr, an_salary) VALUES (3, 'Stefanescu','Stefan', '3333333333333', '0744444444', 2300);
 
-INSERT INTO Client VALUES ('Cristian', 'Cristi',  '4444444444444', '0755555555');
-INSERT INTO Client VALUES ('Mihailescu', 'Mihai', '5555555555555', '0766666666');
-INSERT INTO Client VALUES ('Dorian', 'Dorin',	  '6666666666666', '0777777777');
+DELETE FROM client;
+INSERT INTO client (cl_id, cl_name, cl_surname, cl_num_personal_code, cl_phone_nr) VALUES (1, 'Cristian', 'Cristi',  '4444444444444', '0755555555');
+INSERT INTO Client (cl_id, cl_name, cl_surname, cl_num_personal_code, cl_phone_nr) VALUES (2, 'Mihailescu', 'Mihai', '5555555555555', '0766666666');
+INSERT INTO Client (cl_id, cl_name, cl_surname, cl_num_personal_code, cl_phone_nr) VALUES (3, 'Dorian', 'Dorin',	  '6666666666666', '0777777777');
 
 INSERT INTO account (cn_client_id_fk, cn_angajat_id_fk, cn_balance) VALUES (3, 2, 5000);
 INSERT INTO account (cn_client_id_fk, cn_angajat_id_fk, cn_balance) VALUES (1, 3, 50);
-INSERT INTO account (cn_client_id_fk, cn_angajat_id_fk, cn_balance) VALUES (2, 1,	600);
+INSERT INTO account (cn_client_id_fk, cn_angajat_id_fk, cn_balance) VALUES (2, 1, 600);
+
